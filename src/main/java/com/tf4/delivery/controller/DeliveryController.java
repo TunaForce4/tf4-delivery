bd7e0f08713f07e2ce3076008b77b9161b5cacfe
dayaelee <dayae.dev@gmail.com>
2025-08-20 07:05:04 +0900

    feat: 배송 부분 수정 PATCH 추가 및 삭제 api 구현

--- src/main/java/com/tf4/delivery/controller/DeliveryController.java
+++ src/main/java/com/tf4/delivery/controller/DeliveryController.java
@@ -1,6 +1,7 @@
 package com.tf4.delivery.controller;
 
 import com.tf4.delivery.dto.request.DeliveryCreateRequestDto;
+import com.tf4.delivery.dto.request.DeliveryPatchRequestDto;
 import com.tf4.delivery.dto.response.DeliveryCreateResponseDto;
 import com.tf4.delivery.dto.response.DeliveryResponseDto;
 import com.tf4.delivery.dto.response.PageResponse;
@@ -15,6 +16,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.UUID;
+
 @RestController
 @RequestMapping("/deliveries")
 @RequiredArgsConstructor
@@ -24,10 +27,9 @@ public class DeliveryController {
 
     // 배송 목록 조회 및 검색
     @GetMapping
-    public ResponseEntity<PageResponse<DeliveryResponseDto>> getDeliveries(@PageableDefault(page = 0, size = 10,
-                                sort = "updatedAt", direction = Sort.Direction.DESC)
-                                Pageable pageable,
-                                @RequestParam(value = "q", required = false) String q
+    public ResponseEntity<PageResponse<DeliveryResponseDto>> getDeliveries(
+            @PageableDefault(page = 0, size = 10, sort = "updatedAt", direction = Sort.Direction.DESC)Pageable pageable,
+            @RequestParam(value = "q", required = false) String q
     ){
         Page<DeliveryResponseDto> page = deliveryService.searchDeliveries(q, pageable);
         return ResponseEntity.ok(PageResponse.of(page));
@@ -35,13 +37,26 @@ public class DeliveryController {
 
     // 배송 생성(주문시 자동 생성)
     @PostMapping
-    public ResponseEntity<DeliveryCreateResponseDto> createDeliveries(@Validated
-                                                                      @RequestBody DeliveryCreateRequestDto requestDto){
+    public ResponseEntity<DeliveryCreateResponseDto> createDelivery(
+            @Validated
+            @RequestBody DeliveryCreateRequestDto requestDto){
         return new ResponseEntity<>(deliveryService.createDelivery(requestDto), HttpStatus.CREATED);
     }
 
     // 배송 수정
-
+    @PatchMapping("/{deliveryId}")
+    public ResponseEntity<DeliveryResponseDto> patchDelivery (
+            @PathVariable UUID deliveryId,
+            @Validated
+            @RequestBody DeliveryPatchRequestDto requestDto){
+        return new ResponseEntity<>(deliveryService.patchDelivery(deliveryId, requestDto), HttpStatus.OK);
+    }
 
     // 배송 삭제
+    @DeleteMapping("/{deliveryId}")
+    public ResponseEntity<Void> deleteDelivery(@PathVariable UUID deliveryId){
+        deliveryService.deleteDelivery(deliveryId);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
 }
