a4f794407af64816e21f34cf82770bb2009bd1bf
dayaelee <dayae.dev@gmail.com>
2025-08-21 03:33:36 +0900

    feat: 허브간 배송 경로 생성 요청 DTO 추가

---
+++ src/main/java/com/tf4/delivery/dto/request/DeliveryRouteLegCreateRequestDto.java
@@ -0,0 +1,22 @@
+package com.tf4.delivery.dto.request;
+
+import lombok.Getter;
+
+import java.math.BigInteger;
+import java.util.UUID;
+
+@Getter
+public class DeliveryRouteLegCreateRequestDto {
+
+    private UUID routeLegId;
+    private UUID deliveryId;
+    private UUID departureHubId;
+    private UUID arrivalHubId;
+    private Double estimatedDistanceKm;
+    private Double estimatedTimeMin;
+    private Double actualDistanceKm;
+    private Double actualTimeMin;
+    private String status;
+    private BigInteger hubDeliveryAgentId;
+
+}
