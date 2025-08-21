9dfedd667e7bc751b56ad2efcad047f1d7f930dd
dayaelee <dayae.dev@gmail.com>
2025-08-21 16:34:40 +0900

    chore: 임시 값 삭제

--- src/main/java/com/tf4/delivery/dto/request/DeliveryCreateRequestDto.java
+++ src/main/java/com/tf4/delivery/dto/request/DeliveryCreateRequestDto.java
@@ -10,7 +10,4 @@ public class DeliveryCreateRequestDto {
     private UUID id;
     private UUID supplyCompanyId;
     private UUID receiveCompanyId;
-    // 아래는 임시 값. 실제 값 받아오는 api 완성되면 삭제 예정
-    private BigInteger tmp;
-    private String tmpString;
 }
