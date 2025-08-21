67f77659b33424d504f70e1ffbd586381f2b1bf9
dayaelee <dayae.dev@gmail.com>
2025-08-21 16:23:55 +0900

    feat: 업체 서비스 연동용 Feign 클라이언트 추가

---
+++ src/main/java/com/tf4/delivery/repository/feign/company/CompanyFeignClient.java
@@ -0,0 +1,20 @@
+package com.tf4.delivery.repository.feign.company;
+
+import com.tf4.delivery.repository.feign.company.response.CompanyFindInfoResponseDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.UUID;
+
+@FeignClient(
+        name = "companies",
+        url = "localhost:3360",
+        path = "/internal/companies/delivery-company",
+        fallbackFactory = CompanyFeignFallbackFactory.class)
+public interface CompanyFeignClient {
+
+
+    @GetMapping("/{companyId}")
+    CompanyFindInfoResponseDto getCompanyInfo(@PathVariable("companyId") UUID companyId);
+}
