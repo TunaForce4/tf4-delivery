c08d8c70d24e0c940662c70b6e52bf7181f310b1
dayaelee <dayae.dev@gmail.com>
2025-08-22 20:11:08 +0900

    fix: 필요한 필드 추가

--- src/main/java/com/tf4/delivery/repository/feign/auth/response/AuthFindInfoResponseDto.java
+++ src/main/java/com/tf4/delivery/repository/feign/auth/response/AuthFindInfoResponseDto.java
@@ -1,10 +1,20 @@
 package com.tf4.delivery.repository.feign.auth.response;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import java.util.UUID;
 
 @Getter
+@Setter
+@NoArgsConstructor
 public class AuthFindInfoResponseDto {
-    private String role;
+    private UUID userId;
+    private String userLoginId;
+    private String username;
+    private String password;
+    private UserRole role;
     private String slackId;
+    private String tel;
 }
