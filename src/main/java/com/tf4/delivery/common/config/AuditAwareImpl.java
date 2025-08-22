69c6dca135e983a8584dc995d516d977159f2a1d
dayaelee <dayae.dev@gmail.com>
2025-08-22 20:12:05 +0900

    chore: 헤더에 유저아이디 값 없어도 빈 값 리턴하지 않게끔 설정 추가

--- src/main/java/com/tf4/delivery/common/config/AuditAwareImpl.java
+++ src/main/java/com/tf4/delivery/common/config/AuditAwareImpl.java
@@ -2,6 +2,7 @@ package com.tf4.delivery.common.config;
 
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.data.domain.AuditorAware;
+import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestAttributes;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
@@ -9,6 +10,7 @@ import org.springframework.web.context.request.ServletRequestAttributes;
 import java.util.Optional;
 import java.util.UUID;
 
+@Component("auditorAware")
 public class AuditAwareImpl implements AuditorAware<UUID> {
 
     @Override
@@ -19,15 +21,14 @@ public class AuditAwareImpl implements AuditorAware<UUID> {
         if (requestAttributes instanceof ServletRequestAttributes) {
             ServletRequestAttributes servletRequestAttributes = (ServletRequestAttributes) requestAttributes;
             HttpServletRequest request = servletRequestAttributes.getRequest();
-            String userId = request.getHeader("X-Auth-User-Id");
+            String userId = request.getHeader("X-User-Id");
 
-            if(userId == null){
-                return Optional.empty();
+            if (userId != null && !userId.isEmpty()) {
+                return Optional.of(UUID.fromString(userId));
             }
-
-            return Optional.of(UUID.fromString(userId));
         }
 
-        return Optional.empty();
+        return Optional.of(UUID.fromString("00000000-0000-0000-0000-000000000000"));
+
     }
 }
