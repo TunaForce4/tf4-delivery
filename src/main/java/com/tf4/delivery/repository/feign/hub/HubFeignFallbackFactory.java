b988d16cabd87f03288b059b9941e4f9dc7972c5
dayaelee <dayae.dev@gmail.com>
2025-08-23 06:15:56 +0900

    feat: 허브 서비스와 http 통신시 페인클라이언트 fallback설정

---
+++ src/main/java/com/tf4/delivery/repository/feign/hub/HubFeignFallbackFactory.java
@@ -0,0 +1,13 @@
+package com.tf4.delivery.repository.feign.hub;
+
+import org.springframework.cloud.openfeign.FallbackFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class HubFeignFallbackFactory implements FallbackFactory<HubFeignClient> {
+
+    @Override
+    public HubFeignClient create(Throwable cause) {
+        return null;
+    }
+}
