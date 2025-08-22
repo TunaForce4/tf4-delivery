5c40ac2883c5fcf6eccb37820c4a0c38b21f7082
dayaelee <dayae.dev@gmail.com>
2025-08-23 06:12:53 +0900

    chore: 요청시 필요한 필드 간소화

--- src/main/java/com/tf4/delivery/dto/request/DeliveryRouteLegCreateRequestDto.java
+++ src/main/java/com/tf4/delivery/dto/request/DeliveryRouteLegCreateRequestDto.java
@@ -1,21 +1,19 @@
 package com.tf4.delivery.dto.request;
 
+import com.tf4.delivery.dto.response.DeliveryRouteLegResponseDto;
+import com.tf4.delivery.entity.DeliveryRouteLeg;
+import lombok.Builder;
 import lombok.Getter;
 
 import java.util.UUID;
 
 @Getter
+@Builder
 public class DeliveryRouteLegCreateRequestDto {
-
-    private UUID routeLegId;
+//
+//    private UUID routeLegId;
     private UUID deliveryId;
     private UUID departureHubId;
     private UUID arrivalHubId;
-    private Double estimatedDistanceKm;
-    private Double estimatedTimeMin;
-    private Double actualDistanceKm;
-    private Double actualTimeMin;
-    private String status;
-    private UUID hubDeliveryAgentId;
 
 }
